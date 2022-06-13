bcd32a6b726429f8f155c41baade75fefa308293
Esther Manrique <esther_manrique@epam.com>
2022-06-13 18:06:48 -0500

    Added header and main-nav components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
